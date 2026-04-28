350ee99f32271ef235e5bfea1fed688ea4bc7c87
jessicacca62 <jessica.szenga@gmail.com>
2026-04-28 14:47:09 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBlGAWVLfQsbV4jj5CZi-Q5zQMMlyCWHTU",
+  authDomain: "trip-split-7b63d.firebaseapp.com",
+  projectId: "trip-split-7b63d",
+  storageBucket: "trip-split-7b63d.firebasestorage.app",
+  messagingSenderId: "177263668970",
+  appId: "1:177263668970:web:6e9938f5288eb51a033193",
+  measurementId: "G-H0WLMT0PBJ"
+};
